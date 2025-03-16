updated : neural network because earlier one was incorrect: used softmax instead of sigmoid

src/neural_network.py
@@ -56,9 +56,9 @@ class NeuralNetwork(nn.Module):
         # layers list with correct values for parameters in_features and
         # out_features. This is the last layer of the network.
         # HINT: You will use self.list_hidden here.
-        layers.append(torch.nn.Linear(in_features=self.list_hidden[-1], out_features=self.num_classes))
+        layers.append(torch.nn.Linear(in_features=self.list_hidden[-1], out_features=1))
         
-        layers.append(nn.Softmax(dim=1))
+        layers.append(nn.Sigmoid())
         self.layers = nn.Sequential(*layers)
 
         return self.layers
@@ -158,6 +158,45 @@ class NeuralNetwork(nn.Module):
 
         return x, probabilities
 
+    # def forward(self,
+    #             x,
+    #             verbose=False):
+    #     """Forward propagation of the model, implemented using PyTorch.
+
+    #     Arguments:
+    #         x {torch.Tensor} -- A Tensor of shape (N, D) representing input
+    #         features to the model.
+    #         verbose {bool, optional} -- Indicates if the function prints the
+    #         output or not.
+
+    #     Returns:
+    #         torch.Tensor, torch.Tensor -- A Tensor of shape (N, C) representing
+    #         the output of the final linear layer in the network. A Tensor of
+    #         shape (N, C) representing the probabilities of each class given by
+    #         the softmax function.
+    #     """
+
+    #     # For each layer in the network
+    #     for i in range(len(self.layers) - 1):
+
+    #         # Call the forward() function of the layer
+    #         # and return the result to x.
+    #         x = self.layers[i](x)
+
+    #         if verbose:
+    #             # Print the output of the layer
+    #             print('Output of layer ' + str(i))
+    #             print(x, '\n')
+
+    #     # Apply the softmax function
+    #     probabilities = self.layers[-1](x)
+
+    #     if verbose:
+    #         print('Output of layer ' + str(len(self.layers) - 1))
+    #         print(probabilities, '\n')
+
+    #     return x, probabilities
+
     def forward(self,
                 x,
                 verbose=False):
@@ -177,25 +216,19 @@ class NeuralNetwork(nn.Module):
         """
 
         # For each layer in the network
-        for i in range(len(self.layers) - 1):
-
-            # Call the forward() function of the layer
-            # and return the result to x.
-            x = self.layers[i](x)
-
-            if verbose:
-                # Print the output of the layer
-                print('Output of layer ' + str(i))
-                print(x, '\n')
-
-        # Apply the softmax function
-        probabilities = self.layers[-1](x)
+        x = self.layers(x)
 
         if verbose:
-            print('Output of layer ' + str(len(self.layers) - 1))
+                print("Output before sigmoid")
+                print(x, '\n')
+
+        probabilities = torch.sigmoid(x)
+
+        if verbose:
+            print(("Probabilities after sigmoid:"))
             print(probabilities, '\n')
 
-        return x, probabilities
+        return probabilities
 
     def predict(self,
                 probabilities):
@@ -211,4 +244,4 @@ class NeuralNetwork(nn.Module):
         """
 
         # TODO: Return the index of the class with the highest probability
-        return torch.argmax(probabilities, dim=1)
+        return (probabilities >= 0.5).long().squeeze()